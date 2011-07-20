34425ea0ba1e3f596fb69e06399344d83c7cd0f7
Wilson Guimarães <wegneto@gmail.com>
2011-07-20 15:26:27 -0300

    Testing interceptors.
    Change-Id: I226c433f8a5aaeba11ec017b94c7cf94cb2a1481

--- cdijar/src/main/java/examples/MyAnotherBean.java
+++
@@ -1,8 +0,0 @@
-package examples;
-
-public class MyAnotherBean {
-
-	public void method() {
-		System.out.println("do nothing!");
-	}
-}
--- webapp/src/main/java/examples/MyBean.java
+++ webapp/src/main/java/examples/MyBean.java
@@ -1,8 +1,13 @@
 package examples;
 
+import javax.inject.Named;
+
+@Named
 public class MyBean {
 
+	@MyInterceptorBinding
 	public void method() {
-		System.out.println("do nothing!");
+		System.out.println("At this point, an interception should have occurred.");
 	}
+
 }
